5e52697229cfd963948e37c3d256db62c33ae7c9
Божко Александр Викторович <qa_aleksvik@outlook.com>
2021-03-11 08:38:56 +0400

    my chat

---
+++ src/MyChat.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class MyChat extends JFrame {
+    private List<String> chatParticipants;
+    private JTextArea messageOutput;
+    private JTextField messageInput;
+    private JTextField contacts;
+
+    public MyChat() {
+        setBounds(500, 500, 700, 600);
+        setTitle("TEST_CHAT");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        messageOutput = new JTextArea();
+        messageInput = new JTextField("Enter your message");
+        messageOutput.setEditable(false);
+        messageOutput.setLineWrap(true);
+
+        messageOutput.setRows(10);
+        messageOutput.setWrapStyleWord(true);
+
+        contacts = new JTextField();
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        add(bottomPanel, BorderLayout.SOUTH);
+        JButton sendMessage = new JButton("SEND");
+        bottomPanel.add(sendMessage, BorderLayout.EAST);
+
+        JScrollPane jScrollPane = new JScrollPane(messageOutput);
+        add(jScrollPane, BorderLayout.CENTER);
+
+        bottomPanel.add(messageInput, BorderLayout.CENTER);
+
+        // JPanel jPanel = new JPanel(new BorderLayout());
+        // add(jPanel, BorderLayout.WEST);
+        // JComboBox jComboBox = new JComboBox(( ComboBoxModel ) chatParticipants);
+        // jComboBox.add(( Component ) chatParticipants);
+
+        sendMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                if ((!messageInput.getText().trim().isEmpty())) ;
+                messageOutput.append(String.format("> %s\n", messageInput.getText()));
+                messageInput.setText(" ");
+                messageInput.grabFocus();
+            }
+        });
+        add(messageOutput);
+
+        messageInput.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                while ((!messageInput.getText().trim().isEmpty())) {
+                    messageOutput.append(String.format("> %s\n", messageInput.getText()));
+                    messageInput.setText(" ");
+                    messageInput.grabFocus();
+                }
+            }
+        });
+        add(messageOutput);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MyChat();
+    }
+}
